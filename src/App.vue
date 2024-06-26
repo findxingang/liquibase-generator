74d8da1f6705d75a0c9adff22330e94ffe937f66
xingang <findxingang@163.com>
2024-06-26 19:10:21 +0800

    1. 初始化Vue 2.集成Ant Design Vue 3.添加首页布局

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <PageLayout style="min-height: 100vh">
+
+  </PageLayout>
+</template>
+<script setup>
+import {ref} from 'vue';
+import PageLayout from "@/components/PageLayout.vue";
+
+const selectedKeys = ref(['2']);
+</script>
+<style scoped>
+.site-layout-content {
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+}
+
+#components-layout-demo-top .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+[data-theme='dark'] .site-layout-content {
+  background: #141414;
+}
+</style>
